b1f909b6c9d63ab97f39571cb8310197ae93ee89
auto200 <michal.warac@gmail.com>
2021-09-20 14:35:59 +0200

    Fix spacing and add filtering

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,15 @@
-import BeersGrid from "@components/Home/BeersGrid";
 import Footer from "@components/Home/Footer";
 import Header from "@components/Home/Header";
+import { SimpleGrid } from "@chakra-ui/react";
+import BeerGridItem from "@components/Home/BeerGridItem";
 import beersData from "@shared/beers-data.json";
 import { OWNERS_DATA } from "@shared/constants";
 import { AnyBeer } from "@shared/types";
 import { BrandFilterId } from "@utils/types";
 import type { NextPage } from "next";
 import Head from "next/head";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
+import { GridComponents, VirtuosoGrid } from "react-virtuoso";
 
 const Home: NextPage = () => {
   const [filterBrandId, setFilterBrandId] = useState<BrandFilterId>("ALL");
@@ -33,6 +35,18 @@ const Home: NextPage = () => {
     setFiltredBeers(filtredByBrandAndNameBeers);
   }, [filterBrandId, filterBeerName]);
 
+  const components = useMemo(() => {
+    return {
+      List: React.forwardRef<HTMLDivElement>(({ children, ...props }, ref) => {
+        return (
+          <SimpleGrid minChildWidth="320px" mt="20px" ref={ref} {...props}>
+            {children}
+          </SimpleGrid>
+        );
+      }),
+    };
+  }, []);
+
   return (
     <>
       <Head>
@@ -43,7 +57,15 @@ const Home: NextPage = () => {
         setFilterBrandId={setFilterBrandId}
         setFilterBeerName={setFilterBeerName}
       />
-      <BeersGrid beers={filtredBeers} />
+      <VirtuosoGrid
+        useWindowScroll
+        totalCount={filtredBeers.length}
+        overscan={200}
+        components={components as GridComponents}
+        itemContent={(index) => (
+          <BeerGridItem beer={filtredBeers[index] as AnyBeer} />
+        )}
+      />
       <Footer />
     </>
   );
